1ba257fed306fec076d4a6006cbd41915df6e7b4
Tomas Bance <kalianasu@gmail.com>
2022-12-14 18:41:51 +0100

    29:40 - Start to build first .NET MAUI app - Main view

--- Part 1 - Displaying Data/MonkeyFinder/Model/Monkey.cs
+++ Part 1 - Displaying Data/MonkeyFinder/Model/Monkey.cs
@@ -2,4 +2,13 @@
 
 public class Monkey
 {
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Details { get; set; }
+    public string Image { get; set; }
+    public int Population { get; set; }
+    public float Latitude { get; set; }
+    public float Longitude { get; set; }
+
+
 }
