c36ede10b5212966d56f9d38b2dc9b7e733178c0
Tomas Bance <kalianasu@gmail.com>
2022-12-15 12:31:12 +0100

    01:22:30 - .NET Community Toolkit for MVVM (Source Generators)- Short Code Version

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,53 +1,23 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
-public class BaseViewModel : INotifyPropertyChanged
+namespace MonkeyFinder.ViewModel;
+
+
+public partial class BaseViewModel : ObservableObject
 {
+    public BaseViewModel() 
+    {
+    }
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
     bool isBusy;
+
+    [ObservableProperty]
     string title;
 
 
-    public bool IsBusy
-    {
-        get => isBusy; 
-        set
-        {
-            if (isBusy == value) {
-                return;
-            }
-            isBusy = value;
-
-            OnPropertyChanged();
-            OnPropertyChanged(nameof(IsNotBusy));
-            // OnPropertyChanged("isBusy"); // simple way
-            // OnPropertyChanged(nameof(isBusy)); // returns name in the way of string of this property or method
-            
-        }
-    }
-    public string Title
-    {
-        get => title;
-        set
-        { 
-            if (title == value) 
-                return;
-             
-            title = value;
-
-            OnPropertyChanged();
-        }
-    }
-
     public bool IsNotBusy => !IsBusy;
 
-    public event PropertyChangedEventHandler PropertyChanged;
-
-
-    // public void OnPropertyChanged(string propertyName)
-    // Get a caller member name wich is isBusy then no need OnPropertyChanged(pass a string)
-    public void OnPropertyChanged([CallerMemberName]string propertyName =null) // run time copiler name
-    {
-        // does null check and if anything is subscribed to this event if so then invoke it
-        // it pass this it self what the view model is and pass in that specific property name
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
+   
 }
